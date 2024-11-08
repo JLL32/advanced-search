62788364981dfc916b829b9cbb6309c02a795b01
JALAL <eabduljalil@outlook.com>
2024-11-08 15:42:11 +0100

    add tests

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -5,120 +5,55 @@ import (
 	"testing"
 )
 
-func TestNextToken(t *testing.T) {
-	input := `let five = 5;
-let ten = 10;
-
-let add = fn(x, y) {
-  x + y;
-};
-
-let result = add(five, ten);
-!-/*5;
-5 < 10 > 5;
-
-if (5 < 10) {
-	return true;
-} else {
-	return false;
+var tests = []struct {
+	input    string
+	expected []token.Token
+}{
+	{
+		input: `size >= 1000kb type = pe fs < 2020-12-31 name = "example" positives != 0`,
+		expected: []token.Token{
+			{token.SIZE, "size", token.TYPE_INT},
+			{token.GE, ">=", ""},
+			{token.INT, "1000", token.TYPE_INT},
+			{token.KB, "kb", ""},
+			{token.TYPE, "type", token.TYPE_ENUM},
+			{token.ASSIGN, "=", ""},
+			{token.STRING, "pe", token.TYPE_STRING},
+			{token.FS, "fs", token.TYPE_DATE},
+			{token.LT, "<", ""},
+			{token.DATE, "2020-12-31", token.TYPE_DATE},
+			{token.NAME, "name", token.TYPE_STRING},
+			{token.ASSIGN, "=", ""},
+			{token.STRING, "example", token.TYPE_STRING},
+			{token.POSITIVES, "positives", token.TYPE_INT},
+			{token.NOT_EQ, "!=", ""},
+			{token.INT, "0", token.TYPE_INT},
+			{token.EOF, "", ""},
+		},
+	},
 }
 
-10 == 10;
-10 != 9;
-`
-
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
-		{token.LET, "let"},
-		{token.IDENT, "five"},
-		{token.ASSIGN, "="},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "ten"},
-		{token.ASSIGN, "="},
-		{token.INT, "10"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "add"},
-		{token.ASSIGN, "="},
-		{token.FUNCTION, "fn"},
-		{token.LPAREN, "("},
-		{token.IDENT, "x"},
-		{token.COMMA, ","},
-		{token.IDENT, "y"},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.IDENT, "x"},
-		{token.PLUS, "+"},
-		{token.IDENT, "y"},
-		{token.SEMICOLON, ";"},
-		{token.RBRACE, "}"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "result"},
-		{token.ASSIGN, "="},
-		{token.IDENT, "add"},
-		{token.LPAREN, "("},
-		{token.IDENT, "five"},
-		{token.COMMA, ","},
-		{token.IDENT, "ten"},
-		{token.RPAREN, ")"},
-		{token.SEMICOLON, ";"},
-		{token.BANG, "!"},
-		{token.MINUS, "-"},
-		{token.SLASH, "/"},
-		{token.ASTERISK, "*"},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.INT, "5"},
-		{token.LT, "<"},
-		{token.INT, "10"},
-		{token.GT, ">"},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.IF, "if"},
-		{token.LPAREN, "("},
-		{token.INT, "5"},
-		{token.LT, "<"},
-		{token.INT, "10"},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RETURN, "return"},
-		{token.TRUE, "true"},
-		{token.SEMICOLON, ";"},
-		{token.RBRACE, "}"},
-		{token.ELSE, "else"},
-		{token.LBRACE, "{"},
-		{token.RETURN, "return"},
-		{token.FALSE, "false"},
-		{token.SEMICOLON, ";"},
-		{token.RBRACE, "}"},
-		{token.INT, "10"},
-		{token.EQ, "=="},
-		{token.INT, "10"},
-		{token.SEMICOLON, ";"},
-		{token.INT, "10"},
-		{token.NOT_EQ, "!="},
-		{token.INT, "9"},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
-	}
-
-	l := New(input)
-
+func TestNextToken(t *testing.T) {
 	for i, tt := range tests {
-		tok := l.NextToken()
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Type)
-		}
+		l := New(tt.input)
 
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-				i, tt.expectedLiteral, tok.Literal)
+		for j, expectedToken := range tt.expected {
+			tok := l.NextToken()
+
+			if tok.Type != expectedToken.Type {
+				t.Fatalf("tests[%d][%d] - tokentype wrong. expected=%q, got=%q",
+					i, j, expectedToken.Type, tok.Type)
+			}
+
+			if tok.Literal != expectedToken.Literal {
+				t.Fatalf("tests[%d][%d] - literal wrong. expected=%q, got=%q",
+					i, j, expectedToken.Literal, tok.Literal)
+			}
+
+			if tok.ValueType != expectedToken.ValueType {
+				t.Fatalf("tests[%d][%d] - valuetype wrong. expected=%q, got=%q",
+					i, j, expectedToken.ValueType, tok.ValueType)
+			}
 		}
 	}
 }
