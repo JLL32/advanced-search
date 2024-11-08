8786debda7f45ecf3b116732c4a1ee9b76cbfe8a
JALAL <eabduljalil@outlook.com>
2024-11-08 16:24:55 +0100

    add another test case

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -15,7 +15,7 @@ var tests = []struct {
 			{token.SIZE, "size", token.TYPE_INT},
 			{token.GE, ">=", ""},
 			{token.INT, "1000", token.TYPE_INT},
-			{token.KB, "kb", ""},
+			{token.UNIT, "kb", ""},
 			{token.TYPE, "type", token.TYPE_ENUM},
 			{token.ASSIGN, "=", ""},
 			{token.STRING, "pe", token.TYPE_STRING},
@@ -31,29 +31,52 @@ var tests = []struct {
 			{token.EOF, "", ""},
 		},
 	},
+	{
+		input: `type = elf and fs >= 2021-01-01T00:00:00Z or positives < 5`,
+		expected: []token.Token{
+			{token.TYPE, "type", token.TYPE_ENUM},
+			{token.ASSIGN, "=", ""},
+			{token.STRING, "elf", token.TYPE_STRING},
+			{token.AND, "and", ""},
+			{token.FS, "fs", token.TYPE_DATE},
+			{token.GE, ">=", ""},
+			{token.DATE, "2021-01-01T00:00:00Z", token.TYPE_DATE},
+			{token.OR, "or", ""},
+			{token.POSITIVES, "positives", token.TYPE_INT},
+			{token.LT, "<", ""},
+			{token.INT, "5", token.TYPE_INT},
+			{token.EOF, "", ""},
+		},
+	},
 }
 
 func TestNextToken(t *testing.T) {
 	for i, tt := range tests {
-		l := New(tt.input)
 
-		for j, expectedToken := range tt.expected {
-			tok := l.NextToken()
+		tt := tt
+		t.Run(tt.input, func(t *testing.T) {
+			l := New(tt.input)
+			t.Parallel()
 
-			if tok.Type != expectedToken.Type {
-				t.Fatalf("tests[%d][%d] - tokentype wrong. expected=%q, got=%q",
-					i, j, expectedToken.Type, tok.Type)
+			for j, expectedToken := range tt.expected {
+				tok := l.NextToken()
+
+				if tok.Type != expectedToken.Type {
+					t.Fatalf("tests[%d][%d] - tokentype wrong. expected=%q, got=%q",
+						i, j, expectedToken.Type, tok.Type)
+				}
+
+				if tok.Literal != expectedToken.Literal {
+					t.Fatalf("tests[%d][%d] - literal wrong. expected=%q, got=%q",
+						i, j, expectedToken.Literal, tok.Literal)
+				}
+
+				if tok.ValueType != expectedToken.ValueType {
+					t.Fatalf("tests[%d][%d] - valuetype wrong. expected=%q, got=%q",
+						i, j, expectedToken.ValueType, tok.ValueType)
+				}
 			}
 
-			if tok.Literal != expectedToken.Literal {
-				t.Fatalf("tests[%d][%d] - literal wrong. expected=%q, got=%q",
-					i, j, expectedToken.Literal, tok.Literal)
-			}
-
-			if tok.ValueType != expectedToken.ValueType {
-				t.Fatalf("tests[%d][%d] - valuetype wrong. expected=%q, got=%q",
-					i, j, expectedToken.ValueType, tok.ValueType)
-			}
-		}
+		})
 	}
 }
